4f68527827fee1c8e00d3fc57a0c86d8debf5035
mathc <matheoskskb.dev@gmail.com>
2025-10-01 21:45:00 -0300

    chore(server): configure environment profiles for security and database

--- server/src/main/java/br/com/fastgondolas/fastos/server/config/SecurityConfig.java
+++ server/src/main/java/br/com/fastgondolas/fastos/server/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fastgondolas.fastos.server.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
